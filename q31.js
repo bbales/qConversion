f006db456407a1132c53ebd13da4417705ec5ab0
bbales <ben.bales.dev@gmail.com>
2016-11-08 14:08:19 -0500

    removed unused stuff at top

--- q31.js
+++ q31.js
@@ -2,18 +2,14 @@ String.prototype.replaceAt = function(index, character) {
     return this.substr(0, index) + character + this.substr(index + character.length);
 }
 
-var num = '';
-for (var i = 0; i < 32; i++) num = num.concat('1');
-num = makePositive(num);
-
-num = '11111111111111111111111111111111';
-
 function strToS32(strVal) {
-    if (strVal[0] == '0')
-        return parseInt(strVal.substr(1, 31), 2);
+    if (strVal[0] == '0') return parseInt(strVal.substr(1, 31), 2);
+
+    // Two's complement conversion
     // Invert
-    for (var i = 1; i < 32; i++) strVal = strVal.replaceAt(i, (strVal[i] == '1' ? '0' : '1'));
-    // Add 1
+    for (var i = 1; i < 32; i++) strVal = strVal.replaceAt(i, signChar(strVal));
+
+    // Add 1 - ripple carry method
     var carry = 1;
     for (var i = 31; i > 0; i--) {
         // if its 0, invert and stop
@@ -22,25 +18,29 @@ function strToS32(strVal) {
             carry = 0;
             break;
         }
-        // if its a 1, invert and continue
+        // if its 1, invert and continue
         strVal = strVal.replaceAt(i, ('0'));
     }
+
+    // Deal with bit overflow
     if (carry)
         for (var i = 1; i < 32; i++) strVal = strVal.replaceAt(i, '0');
+
+    // Parse and make negative
     return -1 * (parseInt(strVal.substr(1, 31), 2));
 }
 
 function strToQ31(strVal) {
     var result = 0.0;
     for (var i = 1; i < 32; i++) {
-        result += ((strVal[i] == (strVal[0] == '1' ? '1' : '0')) ? 0 : 1) * Math.pow(2, -1 * i);
+        result += ((strVal[i] == signChar(strVal)) ? 1 : 0) * Math.pow(2, -1 * i);
     }
     return (strVal[0] == '0' ? 1 : -1) * result;
 }
 
 function S32ToStr(S32) {
-    var baseStr = (S32 >= 0) ? S32.toString(2) : (S32 >>> 0).toString(2);
     var pad = '';
+    var baseStr = (S32 >= 0 ? S32 : S32 >>> 0).toString(2);
     for (var i = 0; i < 32 - baseStr.length; i++) pad = pad.concat((S32 >= 0) ? '0' : '1');
     return pad.concat(baseStr);
 }
@@ -58,8 +58,11 @@ function makePositive(strVal) {
 }
 
 function flipSign(strVal) {
-    // Negative has MSB as a 1
-    return strVal.replaceAt(0, (strVal[0] == '1' ? '0' : '1'));
+    return strVal.replaceAt(0, signChar(strVal));
+}
+
+function signChar(strVal) {
+    return strVal[0] == '1' ? '0' : '1';
 }
 
 console.clear();
